1131ad49d101669046f9367094ef6490b8b84a09
Abdeslam_An <abdeslamanssar@gmail.com>
2026-01-28 16:17:01 +0100

    Return data from Model

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
     function index()  {
-        return view('job/index');
+        $data = Job::all();
+        return view('job/index', ['jobs' => $data, 'name' => 'AbdeslamAn']);
     }
 }
