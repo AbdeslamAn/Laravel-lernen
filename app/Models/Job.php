dcf544b0cb32f43f1a9827fb615019be51dfd67e
Abdeslam_An <abdeslamanssar@gmail.com>
2026-01-28 16:15:46 +0100

    Create static fn all()

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model
+class Job
 {
-    //
+    public static function all(){
+        return [['title' => 'Softwere', 'salary' => '1000$'],
+                ['title' => 'Grafic Disigner', 'salary' => '2000$']];
+    }
 }
