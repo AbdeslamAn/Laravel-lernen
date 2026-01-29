03e25c52de212d521464247ea35485c7e1b9aefa
Abdeslam_An <abdeslamanssar@gmail.com>
2026-01-29 15:43:46 +0100

    Renamed File welcomme.blade.php to index.blade.php / Datei welcomme.blade.php bennent auf index.blade.php um

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Job Board</title>
+</head>
+<body>
+    <h1>Job Board</h1>
+
+    <nav>
+        <a href="/">Home</a>
+        <a href="/About">About</a>
+        <a href="/Contact">Home</a>
+    </nav>
+</body>
+</html>
