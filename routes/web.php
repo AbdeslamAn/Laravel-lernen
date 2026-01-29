13f9ffc0b9f5d37153167c0b97c8d912fcfe1f99
Abdeslam_An <abdeslamanssar@gmail.com>
2026-01-29 16:14:15 +0100

    Create Route about and contact / Route about und contact erstellen

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
+Route::get('/about', [IndexController::class, 'about']);
+Route::get('/contact', [IndexController::class, 'contact']);
+
 
 Route::get('/job', [JobController::class, 'index'] );
