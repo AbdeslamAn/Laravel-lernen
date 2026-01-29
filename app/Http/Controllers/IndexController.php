461dfc07fccaba45e198318a5358ff332983949d
Abdeslam_An <abdeslamanssar@gmail.com>
2026-01-29 16:16:47 +0100

    Create Function about to return view about and  contact to return view contact

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,4 +9,12 @@ class IndexController extends Controller
     function index(){
         return view('index');
     }
+
+    function about(){
+        return view('about');
+    }
+
+    function contact(){
+        return view('contact');
+    }
 }
