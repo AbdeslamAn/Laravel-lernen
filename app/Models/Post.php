988aa575dadfcbf53e3dd018ddb3b8d201478fd2
Abdeslam_An <abdeslamanssar@gmail.com>
2026-02-03 15:36:06 +0100

    Add  to cannot be updete this field 'id' /  erstellt, um Felder 'id' nicht zu aktualisieren

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,6 @@ class Post extends Model
     //
 
     protected $fillable = ['title', 'body', 'published']; // fields that can be updates
+
+    protected $guarded = ['id']; // cannot be updates/assigned
 }
