56877387182abbf9cbf0fa972c8e906813e70f9f
Abdeslam_An <abdeslamanssar@gmail.com>
2026-02-12 16:07:53 +0100

    Return all Post and show in the page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    function index(){
+        // Eloquent ORM -> Get all data
+        $data = Post::all();
+
+        // Pass the data to the view
+        return view('post.index', ['post' => $data]);
+    }
 }
