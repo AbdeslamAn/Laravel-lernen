87f495d0bc1413ab91252dfe34cf1f96f4844904
Abdeslam_An <abdeslamanssar@gmail.com>
2026-02-13 16:31:25 +0100

    Add column author in  to can be updates / Filder author in  erstellt, um er zu  änderen

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
     //
 
     protected $table = 'post';
-    protected $fillable = ['title', 'body', 'published']; // fields that can be updates
+    protected $fillable = ['title', 'body', 'author', 'published']; // fields that can be updates
 
     protected $guarded = ['id']; // cannot be updates/assigned
 }
