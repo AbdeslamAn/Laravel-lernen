8a86c2b89d0a73df5503c7db3437ad4001ff9871
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-11 16:58:00 +0100

    Changed title to link to show all fields

--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout title="Commment" titlePage="Comment">
     <h2>Comments Page</h2>
-@foreach ($comments as $post)
+@foreach ($comments as $comment)
     <h1>{{ $comment->author}}</h1>
     <p>{{ $comment->content }}</p>
-    <p>{{ $comment->post}}</p>
+    <a href="/blog/{{ $comment->post->id }}"> <p>{{ $comment->post->title}}</p></a>
 @endforeach
 </x-layout>
