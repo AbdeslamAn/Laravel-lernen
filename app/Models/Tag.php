7c59fe41f84d16c426df71202b22a0d40e51c28c
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-12 19:46:12 +0100

    Configure Tag model (add table, fillable, and guarded) / Model Tag konfiguriert (table, fillable und guarded hinzugefügt)

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $table = 'tag';
+
+    protected $fillable = ['title']; // fields that can be updates
+
+    protected $guarded = ['id']; // cannot be updates/assigned
 }
