faaae5d26de0e656e1f575271f57792b5e037965
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-14 15:47:26 +0100

    Add function index to show all tags, create to create new fild and delete to delete fild

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    function index(){
+        // Eloquent ORM -> Get all data
+        $data = Tag::all();
+
+        // Pass the data to the view
+        return view('tag.index', ['tags' => $data]);
+    }
+
+
+    function create(){
+        Tag::create([
+            'title' => 'CSS',
+        ]);
+
+        return redirect('/tags');
+    }
+
+    public function delete(){
+        Tag::destroy(4);
+
+        return redirect('/tags');
+    }
+
+
 }
