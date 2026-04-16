6ad6b0b05a7cbe5d368b8150030b1776e46a5fb0
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-16 16:02:34 +0100

    Create reloation betwenn table tag and post

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -32,4 +32,28 @@ class TagController extends Controller
     }
 
 
+    function testManyToMany(){
+        // $post46 = Post::find(46);
+        // $post51 = Post::find(51);
+
+        // $post46->tags()->attach([1,5]);
+        // $post51->tags()->attach([5]);
+
+        // return response()->json(([
+        //     'post46' => $post46->tags,
+        //     'post51' => $post51->tags
+
+        // ]));
+
+        $tag = Tag::find(1);
+
+        $tag->posts()->attach((46));
+
+        return response()->json(([
+            'tag' => $tag->title,
+            'posts' => $tag->posts
+
+        ]));
+
+    }
 }
