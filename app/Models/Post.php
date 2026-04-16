f91bc98e78eec870f64d51ef9f5ee59e1c5a2846
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-16 16:04:44 +0100

    Add property Primary Key to use UUID- Universel Unique Indentifier

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasUuids;
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string'; // UUID - Universel Unique Identfier
+
+    public $incrementing = false;  
 
     protected $table = 'post';
     protected $fillable = ['title', 'body', 'author', 'published']; // fields that can be updates
