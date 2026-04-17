d77364f0a7cbf916133aa72130578d1142e2635e
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-17 18:09:54 +0100

    Add use Hasfactory to releted the model with PostFactory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    use HasFactory;
+
     use HasUuids;
 
     protected $primaryKey = 'id';
 
     protected $keyType = 'string'; // UUID - Universel Unique Identfier
 
-    public $incrementing = false;  
+    public $incrementing = false;
 
     protected $table = 'post';
     protected $fillable = ['title', 'body', 'author', 'published']; // fields that can be updates
