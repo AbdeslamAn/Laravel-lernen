68234a7f15e7dc3dcde20fd0cf158f9245b38455
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-17 17:38:34 +0100

    Not show Id

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout title="Blog" titlePage="Blog">
     <h2><u>Blog Page:</u></h2><br>
 @foreach ($posts as $post)
-    <h1>{{ $post->id }}: {{ $post->title}}</h1>
+    <h1>{{ $post->title}}</h1>
     <p>{{ $post->body }}</p>
     <p>{{ $post->author }}</p>
 @endforeach
