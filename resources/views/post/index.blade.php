d4ecae6a1b0ee0f23cb7811b65e8ed6e7fdbe6ab
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-17 16:52:52 +0100

    Add function  Links to show Button Paginate

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,8 +1,10 @@
 <x-layout title="Blog" titlePage="Blog">
-    <h2>Blog Page</h2>
+    <h2><u>Blog Page:</u></h2><br>
 @foreach ($posts as $post)
-    <h1>{{ $post->title}}</h1>
+    <h1>{{ $post->id }}: {{ $post->title}}</h1>
     <p>{{ $post->body }}</p>
     <p>{{ $post->author }}</p>
 @endforeach
+<br>
+{{ $posts->links() }}
 </x-layout>
