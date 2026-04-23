0ae0037a215de6f20dddf0f4b9b135f08e3fa62c
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-23 16:24:56 +0100

    Create Data to Admin User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make(1233)
+
+        ]);
     }
 }
