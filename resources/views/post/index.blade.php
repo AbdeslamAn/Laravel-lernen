2b4327d9e5710000a0b9ee978fd6ca56451805c6
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-23 16:17:14 +0100

    Add style to title and author

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,9 +1,9 @@
 <x-layout title="Blog" titlePage="Blog">
     <h2><u>Blog Page:</u></h2><br>
 @foreach ($posts as $post)
-    <h1>{{ $post->title}}</h1>
+    <h1 class="text-3xl">{{ $post->title}}</h1>
+    <p class="text-2xl">{{ $post->author }}</p>
     <p>{{ $post->body }}</p>
-    <p>{{ $post->author }}</p>
 @endforeach
 <br>
 {{ $posts->links() }}
