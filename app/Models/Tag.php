8f8e039e846d35d465ad65ea3465fe787097e5cd
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-24 20:47:05 +0100

    Add property Primary Key to use UUID- Universel Unique Indentifier

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Tag extends Model
 {
+    use HasFactory;
+
+    use HasUuids;
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string'; // UUID - Universel Unique Identfier
+
+    public $incrementing = false;
     protected $table = 'tag';
 
     protected $fillable = ['title']; // fields that can be updates
