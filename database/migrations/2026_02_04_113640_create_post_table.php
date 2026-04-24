02128e7ee8d491eb2af0f8b0533e58c64e017113
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-24 20:38:53 +0100

    Add Id to use UUID and Field author

--- database/migrations/2026_02_04_113640_create_post_table.php
+++ database/migrations/2026_02_04_113640_create_post_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string("title");
+            $table->string("author");
             $table->string("body");
             $table->boolean("published");
             $table->timestamps();
