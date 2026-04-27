2c32fdfc9bcddb090d82911adf61a465242a363d
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-27 15:23:07 +0100

    Add new CommentController with alle Function I nedd

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,36 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-
-    function index(){
-        // Eloquent ORM -> Get all data
-        $data = Comment::cursorPaginate(3);
-
-        // Pass the data to the view
-        return view('comment.index', ['comments' => $data]);
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
     }
 
-    function show($id){
-        $comment = Comment::findOrFail($id);
-
-        return view('comment.show', ['comment' => $comment]);
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
     }
 
-    function create(){
-        // $comment = Comment::create([
-        //     // 'author' => 'Abdeslam',
-        //     // 'content' => 'This is a Test Comment',
-        //     // 'post_id' => 13
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
 
-        // ]);
-        Comment::factory(5)->create();
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
 
-        return redirect('/comments');
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
