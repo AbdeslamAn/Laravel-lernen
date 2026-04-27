d55f92fb877f65ec9c5ee6d51f808d682acba544
Abdeslam_An <abdeslamanssar@gmail.com>
2026-04-27 15:13:43 +0100

    Add new PostController with alle Function you nedd

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,40 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-
-    function index(){
-        // Eloquent ORM -> Get all data
-        $data = Post::cursorPaginate(4);
-
-        // Pass the data to the view
-        return view('post.index', ['posts' => $data]);
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
     }
 
-    function show($id){
-        $post = Post::findOrFail($id);
-
-        return view('post.show', ['post' => $post]);
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
     }
 
-    function create(){
-        // $post = Post::create([
-        //     'title' => 'My first Post',
-        //     'body' => 'This is my content',
-        //     'author' => 'Abdeslam',
-        //     'published' => true
-        // ]);
-
-        Post::factory(100)->create();
-
-        return redirect('/blog');
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    public function delete($id){
-        Post::destroy($id);
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
