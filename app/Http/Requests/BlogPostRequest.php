c4276283d4bb4a530f093a7b0ca120626fc809d4
Abdeslam_An <abdeslamanssar@gmail.com>
2026-07-19 18:47:44 +0100

    Add Rules to validation the Form withe messages

--- app/Http/Requests/BlogPostRequest.php
+++ app/Http/Requests/BlogPostRequest.php
@@ -3,18 +3,12 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Override;
 
 class BlogPostRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
+   
+     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +16,19 @@ class BlogPostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'bail|required|unique:post',
+            'author' => 'required',
+            'body' => 'required'
+        ];
+    }
+
+    #[Override]
+    public function messages()
+    {
+        return [
+            'title.required' => 'Field is required',
+            'author.required' => 'Field is required',
+            'body.required' => 'Field is required'
         ];
     }
 }
