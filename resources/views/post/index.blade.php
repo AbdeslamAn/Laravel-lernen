f0d947a4b2e3880000860cf390fdc6337f54443a
Abdeslam_An <abdeslamanssar@gmail.com>
2026-07-19 18:50:13 +0100

    Add the place when show Succes flash message

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,4 +1,9 @@
 <x-layout title="Blog" titlePage="Blog">
+    @if (session('success'))
+        <div class="bg-green-50 px-3 py-2 text-green-500">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="mt-6 flex items-center justify-end gap-x-6">
     <a href="/blog/create" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create Now Post</a>
   </div><br>
