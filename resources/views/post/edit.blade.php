66287f1f180694661546e495db395cdb37338007
Abdeslam_An <abdeslamanssar@gmail.com>
2026-07-20 19:13:49 +0100

    Add function  Method PUT to we kann Update Data

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -2,6 +2,7 @@
 
     <form method="POST" action="/blog">
     @csrf
+    @method('PUT')
   <div class="space-y-12">
     <div class="border-b border-gray-900/10 pb-12">
       <h2 class="text-base/7 font-semibold text-gray-900">Edit Post: {{ $post->title }}</h2>
