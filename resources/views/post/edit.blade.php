cdd8b685337a367d29dc467a7006da1108b631a8
Abdeslam_An <abdeslamanssar@gmail.com>
2026-07-20 19:50:42 +0100

    Add eh condition in the filde published

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -23,7 +23,7 @@
         <div class="sm:col-span-3">
           <label for="author" class="block text-sm/6 font-medium text-gray-900">Author</label>
           <div class="mt-2">
-            <input id="author" value="{{ old('title', $post->author) }}" type="text" name="author" autocomplete="family-name" class=" {{ $errors->has('author') ? 'outline-red-500' : 'outline-gray-300'}} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1  placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input id="author" value="{{ old('author', $post->author) }}" type="text" name="author" autocomplete="family-name" class=" {{ $errors->has('author') ? 'outline-red-500' : 'outline-gray-300'}} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1  placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
           </div>
           @error('author')
               <span class="text-red-500">{{ $message }}</span>
@@ -33,7 +33,7 @@
         <div class="col-span-full">
           <label for="body" class="block text-sm/6 font-medium text-gray-900">Content</label>
           <div class="mt-2">
-            <textarea id="body" name="body" rows="3" class=" {{ $errors->has('body') ? 'outline-red-500' : 'outline-gray-300'}} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1  placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">{{ old('title', $post->body) }}</textarea>
+            <textarea id="body" name="body" rows="3" class=" {{ $errors->has('body') ? 'outline-red-500' : 'outline-gray-300'}} block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1  placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">{{ old('body', $post->body) }}</textarea>
           </div>
           <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about the article.</p>
             @error('body')
@@ -45,7 +45,7 @@
             <div class="flex gap-3">
                 <div class="flex h-6 shrink-0 items-center">
                     <div class="group grid size-4 grid-cols-1">
-                    <input id="published" type="checkbox" name="published" value="1" {{ $post->published ? 'checked' : ''}}  aria-describedby="published-description" class="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto" />
+                    <input id="published" type="checkbox" name="published" value="1" {{ old('published') || (!old() && $post->published)  ? 'checked' : ''}}  aria-describedby="published-description" class="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto" />
                     <svg viewBox="0 0 14 14" fill="none" class="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25">
                         <path d="M3 8L6 11L11 3.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-checked:opacity-100" />
                         <path d="M3 7H11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-indeterminate:opacity-100" />
