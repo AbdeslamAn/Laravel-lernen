307652501052d16206c7a434177c35459bc97e37
Abdeslam_An <abdeslamanssar@gmail.com>
2026-09-02 20:57:32 +0100

    Add route Comment

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\api\v1\PostApiController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function(){
     Route::apiResource('post', PostApiController::class);
 });
+
+
+Route::apiResource('comment', CommentController::class);
+
